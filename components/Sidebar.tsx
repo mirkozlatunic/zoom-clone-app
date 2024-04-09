a6277b9fc325573fe6fe7c6936b86d0ec5b7dc10
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-04-08 21:53:23 -0700

    Sidebar part 1

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { cn } from "@/lib/utils";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <section className="sticky left:0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-      <div className="flex flex-1 flex-col gap-6"></div>
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex gap-4 items-centerp-4 rounded-lg justify-start",
+                { "bg-blue-1": isActive }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
