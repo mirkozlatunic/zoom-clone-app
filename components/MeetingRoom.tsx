83462229dd83e6d05f41cd4f61b3e9d3bde35574
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-04-16 22:51:52 -0700

    meeting room logo setup

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-  return <div>MeetingRoom</div>;
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+      case "speaker-left":
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex- size-full max-w-[1000px]">
+          <CallLayout />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MeetingRoom;
