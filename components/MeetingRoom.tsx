7ba44079deb4b1db51ace77ad84fe9d3260ec05c
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-04-17 20:09:06 -0700

    setup of StreamTheme

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,10 +1,17 @@
-import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
 
   const CallLayout = () => {
     switch (layout) {
@@ -23,6 +30,17 @@ const MeetingRoom = () => {
         <div className="flex- size-full max-w-[1000px]">
           <CallLayout />
         </div>
+        <div
+          className={cn("h-[calc(100vh-86px)} hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
       </div>
     </section>
   );
